3df57999b3d2f49cbc40efe99e811ddcaf1e4693
Carlos Garcia <carlosaicrag@gmail.com>
2020-03-28 18:15:04 -0700

    get names of graduating class to show up

--- src/index.js
+++ src/index.js
@@ -422,13 +422,38 @@ document.addEventListener("DOMContentLoaded", () => {
     let canvas = document.getElementById("canvas")
     let nextStudentButton = document.getElementById("next-student")
     let ctx = canvas.getContext("2d")
-    let graduates = []
+    let ol1 = document.getElementById("ol1")
+    let ol2 = document.getElementById("ol2")
+    let submitButton = document.querySelector("#current-student-button")
+
+    let graduates = [] 
     students.forEach((student) => {
         let graduate = new Graduate(ctx,student.name,student.imageUrl)
         graduates.push(graduate)
     })
 
+    let mid = Math.floor(graduates.length/2)
+    ol2.start = mid + 1
+
+    for (let i = 0; i < mid;i++) {
+        let listItem = document.createElement("li")
+        listItem.textContent = graduates[i].name
+        ol1.appendChild(listItem)
+    }
+
+    for (let i = mid; i < graduates.length;i++) {
+        let listItem = document.createElement("li")
+        listItem.textContent = graduates[i].name
+        ol2.appendChild(listItem)
+    }
+
     let ceremony = new Ceremony(ctx, graduates)
+
+    submitButton.addEventListener("click", () => {
+        let input = document.querySelector("input");
+        ceremony.currentStudent = parseInt(input.value) - 1
+    })
+
     ceremony.constructStage()
 
 
